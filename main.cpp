ff643a7b6957c71b8f30c5fcdace690defbd62c8
Matchman <match41@users.sourceforge.net>
2017-04-05 14:55:13 +0800

    don't forget to call Reply()

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
 
-#include <boost/asio.hpp>
+#include "httpd/Server.hh"
+#include "httpd/Response.hh"
 
 int main()
 {
 	std::cout << "Hello, World! " << std::endl;
+
+	boost::asio::io_service ios;
+
+	http::Server s{ios, "0.0.0.0", "8080"};
+	s.SetDefaultHandler([](const http::ConnectionPtr& conn){
+		conn->Response().SetStatus(http::Response::Status::ok);
+
+		std::ostream os{&conn->Response().Content()};
+		os << "hello";
+		conn->Reply();
+	});
+
+	ios.run();
 	return 0;
 }
