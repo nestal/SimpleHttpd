df6fcbf6662afb5a92e4850050f98d4e3faee577
Nestal Wan <me@nestal.net>
2017-11-03 23:43:22 +0800

    Added test case for POST content

--- test/HttpParserTest.cc
+++ test/HttpParserTest.cc
@@ -18,7 +18,7 @@
 
 using namespace http;
 
-TEST_CASE("HttpParser simple test", "[normal]")
+TEST_CASE("HttpParser GET simple request", "[normal]")
 {
 	HttpParser subject;
 	const char request[] = "GET /some/path/to/url HTTP/1.1\r\n"
@@ -56,4 +56,28 @@ TEST_CASE("HttpParser simple test", "[normal]")
 		CHECK(result.Method() == "GET");
 		CHECK(result.Headers().Count() == 3);
 	}
+}
+
+TEST_CASE("HttpParser POST request content", "[normal]")
+{
+	const char request[] =
+		"POST / HTTP/1.1\r\n"
+		"Host: localhost:8080\r\n"
+		"User-Agent: curl/7.53.1\r\n"
+		"Accept: */*\r\n"
+		"Content-Length: 12\r\n"
+		"Content-Type: application/x-www-form-urlencoded\r\n"
+		"\r\n"
+		"hello=world!\r\n"
+	;
+	const auto size = sizeof(request)-1;
+	
+	HttpParser subject;
+	auto r = subject.Parse(request, size);
+	CHECK(r == size);
+	CHECK(subject.Errno() == HPE_OK);
+	
+	auto&& result = subject.Result();
+	CHECK(result.Method() == "POST");
+	CHECK(result.Content() == "hello=world!");
 }
